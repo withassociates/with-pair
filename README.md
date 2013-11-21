5304a72933380be5e388fcb412d46440039e5767
Jamie White <jamie@jgwhite.co.uk>
2013-11-21 12:00:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pair With Associates
+
+Need a hand working through a tricky programming problem?
+Or help finding the best route for a design solution?
+Or just someone to bound ideas off?
+
+Us too.
+
+That’s why we’re making ourselves available for short daily pairing sessions.
+
+http://pair.withassociates.com/
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pair::Application.config.secret_key_base = '418a83a8dfc381f4e12d5c18b325771e723ced8a9f34cc6b86c82941e49c08297b3f95358470a37d9b2f510056bccd6a38d0684c75ac40b0d1dbe702eca96f50'
