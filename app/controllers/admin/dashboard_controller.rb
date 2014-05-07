0829a8c4fa9875050f4392250c6177088ba2af87
Calum Gunn <calum@calumgunn.com>
2014-05-07 16:22:16 +0100

    Associates can edit their skills

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,4 +5,20 @@ class Admin::DashboardController < ApplicationController
     @sessions = current_person.sessions.includes(:bookings)
     @new_session = @sessions.new
   end
+
+  def update
+    if current_person.update_attributes(person_params)
+      flash[:admin] = 'Skills updated!'
+      redirect_to admin_root_path
+    else
+      flash[:admin] = 'There was a problem updating your skills...'
+      render 'show'
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:skills)
+  end
 end
