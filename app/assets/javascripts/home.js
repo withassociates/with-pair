9798a4c13330ab11465fbc12c66273def501fd18
Calum Gunn <calum@calumgunn.com>
2014-06-26 11:52:57 +0100

    User can contact us to schedule times

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  $('#contact-person').change(function() {
+    var email = $('#contact-person').val();
+    var subject = '?subject=Looking to Pair!'
+
+    $('#negotiate-button').attr('href', 'mailto:' + email + subject);
+  });
+
+  var width = $(window).width();
+
+  if (width < 690) {
+    $('option').each(function(i) {
+      var str = $(this).html();
+
+      var newstr = str.replace(/[(].+[)]/g, '');
+      $(this).text(newstr);
+    });
+  }
+});
